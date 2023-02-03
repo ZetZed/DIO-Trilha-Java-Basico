7d818fa0749899523710684b6d57f43534c54779
Zet Zed <rodhaste@hotmail.com>
2023-02-03 10:39:07 -0400

    Estruturas Condicionais

---
+++ Controle-Fluxo/src/CondicionalEncadeada.java
@@ -0,0 +1,17 @@
+//Condicional Encadeada
+
+public class CondicionalEncadeada {
+    public static void main(String[] args) {
+        int nota = 6;
+
+        if(nota >= 7){
+            System.out.println("Aprovado");
+        }
+        else if(nota >=5 && nota < 7){
+            System.out.println("Prova Recuperação");
+        }
+        else{
+            System.out.println("Reprovado");
+        }
+    }
+}
