aa436c784c740ef92e39fae40daf5f1175347a57
Zet Zed <rodhaste@hotmail.com>
2023-02-07 15:18:47 -0400

    Mjölnir

--- Exercícios/Mjölnir/src/Mjölnir.java
+++ Exercícios/Mjölnir/src/Mjölnir.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Mjölnir {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+		int C = leitor.nextInt();
+		String nome;
+		int N;
+
+// TODO: complete os espaços em branco com sua solução para o problema
+
+		for (int i = 0; i < C; i++) {
+			nome = leitor.next();
+			N = leitor.nextInt();
+			if (nome.equals("Thor")) System.out.println("Y");
+			else System.out.println("N");
+		}
+        leitor.close();
     }
 }
